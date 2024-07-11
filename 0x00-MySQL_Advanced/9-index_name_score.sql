1d41d8be2913c8cd777f8f8bb970baed96998ab3
goddycode <odhisgoddy24@gmail.com>
2024-07-11 21:47:59 +0000

    Adding '9-index_name_score.sql ' to '0x00-MySQL_Advanced'

---
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -0,0 +1,3 @@
+-- This script creates an index idx_name_first_score on the table names
+-- The index only includes the first letter of the name and the score
+CREATE INDEX idx_name_first_score ON names (name(1), score);
