3dbf3fca60f04290f8707c5fd9241f5d998127c2
goddycode <odhisgoddy24@gmail.com>
2024-07-19 11:16:57 +0000

    Updating 'web.py' to '0x02-redis_basic' #3

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,34 +1,43 @@
 #!/usr/bin/env python3
+""" Module for caching web page responses and
+    tracking URL access counts using Redis """
+import redis
 import requests
-import time
 from functools import wraps
-from typing import Dict
+from typing import Callable
 
-cache: Dict[str, str] = {}
 
+r = redis.Redis()
+
+
+def url_count(method: Callable) -> Callable:
+    """ decorator to c track how many times a particular URL
+        was accessed in the key "count:{url}" """
+    @wraps(method)
+    def wrapper(url):
+        """ wrapper that incrments the count in Redis """
+        r.incr("count:{}".format(url))
+        r.set(f"count:{url}", 1)
+        return method(url)
+    return wrapper
+
+
+@url_count
 def get_page(url: str) -> str:
-    if url in cache:
-        print(f"Retrieving from cache: {url}")
-        return cache[url]
-    else:
-        print(f"Retrieving from web: {url}")
-        response = requests.get(url)
-        result = response.text
-        cache[url] = result
-        return result
+    """ function that returns the HTML content of a particular URL """
 
-def cache_with_expiration(expiration: int):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            url = args[0]
-            key = f"count:{url}"
-            if key in cache:
-                count, timestamp = cache[key]
-                if time.time() - timestamp > expiration:
-                    result = func(*args, **kwargs)
-                    cache[key] = (count+1, time.time())
-                    return result
-                else:
-                    cache[key] = (count+1, timestamp)
-                    return
+    # Return the cached response from Redis if it exists
+    cached_response = r.get(f"response:{url}")
+    if cached_response:
+        # print("Cache hit")
+        return cached_response.decode('utf-8')
+
+    # Fetch the response if not cached
+    # print("Cache miss")
+    response = requests.get(url)
+
+    # Cache it with an expiration time of 10 seconds
+    key = "response:{}".format(url)
+    r.setex(key, 10, response.text)
+
+    return response.text
