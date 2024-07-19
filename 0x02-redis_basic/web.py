512d5cc89501163a9e5f748fd4dc21608cb37ec8
goddycode <odhisgoddy24@gmail.com>
2024-07-19 11:23:46 +0000

    Updating 'web.py' to '0x02-redis_basic' #5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,35 +1,21 @@
 #!/usr/bin/env python3
-"""Defines a method that implements an expiring web cache and tracker."""
-
-from typing import Callable
-from functools import wraps
+"""
+create a web cach
+"""
 import redis
 import requests
+rc = redis.Redis()
+count = 0
 
 
-def requests_counter(method: Callable) -> Callable:
-    """ Counts how many times a requesthas been made
-    """
-    r = redis.Redis()
-
-    @wraps(method)
-    def wrapper(url):
-        """ wrapper fxn that counts actual no of requests made"""
-        r.incr(f"count:{url}")
-        cached_ = r.get(f"cached:{url}")
-        if cached:
-            return cached.decode('utf-8')
-
-        html = method(url)
-        r.setex(f"cached:{url}", 10, html)
-        return html
-
-    return wrapper
-
-
-@requests_counter
 def get_page(url: str) -> str:
-    """ obtains html content for a given site url and returns it..
-    """
+    """ get a page and cach value"""
+    rc.set(f"cached:{url}", count)
     resp = requests.get(url)
+    rc.incr(f"count:{url}")
+    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
     return resp.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
